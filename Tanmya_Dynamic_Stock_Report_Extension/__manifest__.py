65642a9a24d3deafa0058647dcadeaedff68d6a5
muhammadadnanoukal <101559768+muhammadadnanoukal@users.noreply.github.com>
2022-07-13 13:58:27 +0300

    Add files via upload

---
+++ Tanmya_Dynamic_Stock_Report_Extension/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'ALTANMYA Dynamic Stock Report (In, Out)',
+    'version': '4.0',
+    'summary': 'A Dynamic report for each product, type of transaction, and transaction with retrospective effects',
+    'description': "A Dynamic report that shows both beginning balance and ending balance for each product, type of transaction, and transaction with retrospective effects",
+    'category': 'Inventory/Inventory',
+    'author': 'ALTANMYA - TECHNOLOGY SOLUTIONS',
+    'company': 'ALTANMYA - TECHNOLOGY SOLUTIONS Part of ALTANMYA GROUP',
+    'website': "http://tech.altanmya.net",
+    'depends': ['stock','product'],
+    'data': ['security/ir.model.access.csv','security/sec.xml','views/view_actions.xml','views/view_menu.xml','views/view_pages.xml'],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
